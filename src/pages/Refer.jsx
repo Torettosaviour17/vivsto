79e3f95d4d9613898611d020bcb8b869a1975bf3
Toretto <145860512+Torettosaviour17@users.noreply.github.com>
2025-01-28 11:26:13 +0100

    working on responsevenneee

--- src/pages/Refer.jsx
+++ src/pages/Refer.jsx
@@ -1,23 +1,36 @@
-import React from 'react';
-import { FaArrowLeft } from 'react-icons/fa';
-import { useNavigate } from 'react-router-dom';
-
+import React from "react";
+import { FaArrowLeft } from "react-icons/fa";
+import { useNavigate } from "react-router-dom";
 
 // import { useNavigate } from 'react-router-dom';
 
-
 function Refer() {
-const navigate = useNavigate();
-    
-  return (
+  const navigate = useNavigate();
 
-    <div className="bg-[#1E1E1E] text-white py-4 px-6 rounded-[20px] font-semibold flex flex-col gap-4 mt-8 relative my-[60px] text-center items-center align-middle">
-    <button onClick={() => navigate(-1)}  className="flex items-center pb-2 relative left-[-20%]">
-        <FaArrowLeft className="mr-2 " />
-      </button>
-      <p className="text-gray-300 leading-relaxed text-sm sm:text-base">
-        Invite friends to join the Vivstock community and earn 5% of what they deposit on their first transaction. It's simple—share your unique referral link, and when they sign up and deposit, you both benefit! Start referring today and grow your earnings while helping others discover the world of stock market investing with Vivstock.
-      </p>
+  return (
+    <div>
+      <div className="flex w-full mb-5 text-white relative pt-3 items-center">
+        <button
+          onClick={() => navigate(-1)}
+          className="lg:hidden flex absolute items-center"
+        >
+          <FaArrowLeft className="ml-4" />
+        </button>
+        <div className="flex-grow text-center">
+          <p className="text-[20px]">
+            Refer a Friend to Vivstock and Earn 5% of Their First Deposit!
+          </p>
+        </div>
+      </div>
+      <div>
+        <p className="text-gray-300 leading-relaxed text-sm sm:text-base">
+          Invite friends to join the Vivstock community and earn 5% of what they
+          deposit on their first transaction. It's simple—share your unique
+          referral link, and when they sign up and deposit, you both benefit!
+          Start referring today and grow your earnings while helping others
+          discover the world of stock market investing with Vivstock.
+        </p>
+      </div>
     </div>
   );
 }
